9067fcd4254c59e98edfa27469e044e864445c99
BLACK PHANTOM CYBER <46845458+BLACK-PHANTOM-CYBER@users.noreply.github.com>
2019-01-21 05:37:18 +0700

    Chrome
    Ayo di install ini silahkan install tool auto install cepat aman praktis
    Tanpa perlu di pilih langsung otomatis menginstall tool seperti sqlmap crewbox dll
    Di dlm tool ini terdapat 50 tool karena masih dlm tahap uji ok
    Tunggu versi berikut nya untuk nenambah tool dan memperbaharui tampilan

---
+++ autoinstall.sh
@@ -0,0 +1,170 @@
+﻿blue='\033[34;1m'
+green='\033[32;1m'  
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'                                           
+yellow='\033[33;1m'
+
+echo
+figlet -f standard "         1     "
+sleep3
+clear
+echo
+figlet -f standard "         2     "
+sleep3
+clear
+echo
+figlet -f standard "         3     "
+sleep3
+clear
+echo
+figlet -f standard "         4     "
+sleep3
+clear
+echo
+figlet -f standard "         5     "
+sleep3
+clear
+echo
+
+
+figlet -f standard "AUTO INSTALER"
+echo
+
+
+
+echo $red"===================================="
+echo $yellow"===================================="
+echo $green"===================================="
+echo $blue"===================================="
+echo $purple"CREATED=MR.SPOON"
+echo $cyan"VERSION=V.0.1"
+echo $white"TEAM=BLACK PHANTOM CYBER"
+echo $blue"===================================="
+echo $green"===================================="
+echo yellow"===================================="
+echo $red"===================================="
+echo
+echo
+echo
+
+
+echo $green"======================================="
+echo $white"NOTE:TOOL INI OTOMATIA AKAN MENGINSTAL
+echo $white"TOOL LAIN SECARA OTOMATIS
+echo $green""======================================"
+echo
+echo $blue "MAU LANJUT Y/N"
+read -p "=====>"Y
+echo
+
+
+echo $green"AVARspam"
+git  clone https://github.com/ALX-04/AVARspam
+sleep1
+clear
+
+echo $green"LAZYMUK"
+git clone https://github.com/Gameye98/Lazymux
+sleep1
+clear
+
+echo $green"BOXSERVER"
+git clone https://github.com/Xeit666h05t/BoxServer
+sleep1
+clear
+
+echo $green"UBUNTU"
+git clone https://github.com/Neo-Oli/termux-ubuntu
+sleep
+clear
+
+echo $green"D-TECH"
+git clone https://github.com/shawarkhanethicalhacker/D-TECT
+sleep1
+clear
+
+echo $green"HASSER"
+git clone https://github.com/CiKu370/hasher
+sleep1
+clear
+
+echp $green"RED_HAWK"
+git clone https://github.com/Tuhinshubhra/RED_HAWK
+sleep1
+clear
+
+echo $green"ZIPCRACK"
+git clone https://github.com/CiKu370/zipcrack
+sleep1
+clear
+
+echo $green"IPDDOS"
+git clone https://github.com/CiKu370/ipddos
+sleep1
+clear
+
+echo $red"BOX CODER"
+git clone https://github.com/Xiet666h05t/BoxCoder
+sleep1
+clear
+
+echo $red"BLACK HIDRA"
+git clone https://Gameye98/Black-Hydra
+sleep1
+clear
+
+echo $green"MR.CAKIL"
+git clone https://github.com/mrcakil/Mrcakil
+sleep1
+clear
+
+echo $red"ADMIN FINDER"
+git clone https://github.com/mrcakil/Admin_Finder
+sleep1
+clear
+
+echo $red"GOOGLE DROK"
+git clone https://github.com/XG77Z10/Google-Dork
+sleep1
+clear
+
+echo $red"HAMMER"
+git clone https://github.com/cyweb/hammer
+sleep1
+clear
+
+echo $red"WEEMAN"
+git clone https://github.com/evait-security/weeman
+sleep1
+clear
+
+echo $red"XSHELL"
+git clone https://github. com/Ubaii/Xshell
+sleep1
+clear
+
+echo $red"LITESPAM"
+git clone https://github.com/4L13199/LITESPAM
+sleep1
+clear
+
+echo $red"SPAZsms"
+git clone https://github.com/Gameye98/SpazSMS
+sleep1
+clear
+
+echo $red"RECONDOG"
+git clone https://github.com/UltimateHackers/ReconDog
+sleep1
+clear
+
+echo
+echo $blue"TUNGGU UPDATE SELANJUTNYA BYE BYE :)"
+echo
+
+
+
+
+
